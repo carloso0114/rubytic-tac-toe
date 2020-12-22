1c3a0a5f44592523a4421992adba81c62270de83
Varun Sharma <67771380+Varun2323@users.noreply.github.com>
2020-12-23 02:03:17 +0530

    changes

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,72 @@
-#!/usr/bin/env ruby
-puts 'Hello world!'
+#frozen_string_literal: true
+
+ puts ''
+ print '                 TIC TAC TOE'
+ puts '
+                _________________
+               |     |     |     |
+               |  1  |  2  |  3  |
+               |_____|_____|_____|
+               |     |     |     |
+               |  4  |  5  |  6  |
+               |_____|_____|_____|
+               |     |     |     |
+               |  7  |  8  |  9  |
+               |_____|_____|_____|
+ '
+ puts ''
+ puts 'Player 1 enter your name:'
+ player_one = gets.chomp
+ puts "{player_one} your symbol is 'X'"
+ puts ''
+ puts 'Player 2 enter your name:'
+ player_two = gets.chomp
+ puts "{player_two} your symbol is 'O'"
+ puts ''
+ puts "    ----------LET'S PLAY!----------"
+ puts "{player_one} make your move"
+ puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
+ position = gets.chomp
+ puts "{player_one} the move you made is invalid,  please try again!"
+ position = gets.chomp
+ puts '
+                _________________
+               |     |     |     |
+               |  1  |  2  |  3  |
+               |_____|_____|_____|
+               |     |     |     |
+               |  4  |  5  |  6  |
+               |_____|_____|_____|
+               |     |     |     |
+               |  7  |  8  |  9  |
+               |_____|_____|_____|
+ '
+ puts ''
+ puts "Congs {player_one}, you are the winner!"
+ puts ''
+ puts "It's a draw, you can play again!"
+ puts ''
+  current_player(player_two)
+ puts "{player_two} make your move"
+ puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
+ position = gets.chomp
+ puts ''
+ puts "{player_two} the move you made is invalid, please try again!"
+ position = gets.chomp
+ puts '
+                _________________
+               |     |     |     |
+               |  1  |  2  |  3  |
+               |_____|_____|_____|
+               |     |     |     |
+               |  4  |  5  |  6  |
+               |_____|_____|_____|
+               |     |     |     |
+               |  7  |  8  |  9  |
+               |_____|_____|_____|
+ '
+ puts ''
+ puts "Congs {player_two}, you are the winner!"
+ puts ''
+ puts "It's a draw, you can play again!"
+ puts ''
