b67ec0f9bbe377d47471e834f0fcede5ce52e5db
Varun Sharma <67771380+Varun2323@users.noreply.github.com>
2020-12-23 04:01:35 +0530

    changes added

--- bin/main.rb
+++ bin/main.rb
@@ -1,72 +1,81 @@
-# frozen_string_literal: true
+# !/usr/bin/env ruby
 
 puts ''
-print '                 TIC TAC TOE'
+print '                  TIC TAC TOE'
 puts '
-                _________________
-               |     |     |     |
-               |  1  |  2  |  3  |
-               |_____|_____|_____|
-               |     |     |     |
-               |  4  |  5  |  6  |
-               |_____|_____|_____|
-               |     |     |     |
-               |  7  |  8  |  9  |
-               |_____|_____|_____|
- '
+               _________________
+              |     |     |     |
+              |  1  |  2  |  3  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  4  |  5  |  6  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  7  |  8  |  9  |
+              |_____|_____|_____|
+'
 puts ''
-puts 'Player 1 enter your name:'
+puts 'Player enter your name:'
 player_one = gets.chomp
-puts "{player_one} your symbol is 'X'"
+puts "#{player_one} your symbol is 'X'"
 puts ''
-puts 'Player 2 enter your name:'
+puts 'Player enter your name:'
 player_two = gets.chomp
-puts "{player_two} your symbol is 'O'"
+puts "#{player_two} your symbol is 'O'"
 puts ''
-puts "    ----------LET'S PLAY!----------"
-puts '{player_one} make your move'
-puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
-position = gets.chomp
-puts '{player_one} the move you made is invalid,  please try again!'
-position = gets.chomp
-puts '
-                _________________
-               |     |     |     |
-               |  1  |  2  |  3  |
-               |_____|_____|_____|
-               |     |     |     |
-               |  4  |  5  |  6  |
-               |_____|_____|_____|
-               |     |     |     |
-               |  7  |  8  |  9  |
-               |_____|_____|_____|
- '
-puts ''
-puts 'Congs {player_one}, you are the winner!'
-puts ''
-puts "It's a draw, you can play again!"
-puts ''
-current_player(player_two)
-puts '{player_two} make your move'
-puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
-position = gets.chomp
-puts ''
-puts '{player_two} the move you made is invalid, please try again!'
-position = gets.chomp
-puts '
-                _________________
-               |     |     |     |
-               |  1  |  2  |  3  |
-               |_____|_____|_____|
-               |     |     |     |
-               |  4  |  5  |  6  |
-               |_____|_____|_____|
-               |     |     |     |
-               |  7  |  8  |  9  |
-               |_____|_____|_____|
- '
-puts ''
-puts 'Congs {player_two}, you are the winner!'
-puts ''
-puts "It's a draw, you can play again!"
-puts ''
+puts "            ===============LET'S PLAY!==============="
+game_ended = false
+is_player_one_move = true
+until game_ended
+  if is_player_one_move
+    # current_player(player_one)
+    puts "#{player_one} make your move"
+    puts ''
+    puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
+    puts ''
+    puts "#{player_one} the move you made is invalid, please try again!"
+    puts '
+               _________________
+              |     |     |     |
+              |  1  |  2  |  3  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  4  |  5  |  6  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  7  |  8  |  9  |
+              |_____|_____|_____|
+'
+    puts ''
+    puts "Congs #{player_one}, you are the winner!"
+    puts ''
+    puts "It's a draw, you can play again!"
+    puts ''
+    is_player_one_move = false
+  else
+    # current_player(player_two)
+    puts "#{player_two} make your move"
+    puts 'Select a position from [1, 2, 3, 4, 5, 6, 7, 8, 9]'
+    puts ''
+    puts "#{player_two} the move you made is invalid, please try again!"
+    puts '
+               _________________
+              |     |     |     |
+              |  1  |  2  |  3  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  4  |  5  |  6  |
+              |_____|_____|_____|
+              |     |     |     |
+              |  7  |  8  |  9  |
+              |_____|_____|_____|
+'
+    puts ''
+    puts "Congs #{player_two}, you are the winner!"
+    puts ''
+    puts "It's a draw, you can play again!"
+    puts ''
+    is_player_one_move = true
+    game_ended = true
+  end
+end
