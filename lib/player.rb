0b8c0e44deffc3e6add4a2bae741b5a342e2309e
carloso0114 <carlossospinag@gmail.com>
2020-12-29 16:26:37 -0500

    fix rubocop offenses

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
 class Player
-    attr_accessor :name
-    attr_reader :symbol
-  
-    def initialize(name, symbol)
-      @name = name
-      @symbol = symbol
-    end
+  attr_accessor :name
+  attr_reader :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
   end
+end
